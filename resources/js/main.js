3227d0f6a69feff88a6c0510fa36b26256e66651
Anas F <anas.fouad@etudiant.univ-brest.fr>
2024-11-17 01:52:54 +0100

    redesign

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,51 +1,10 @@
-// Nav bar and tel transition
-const navbar = document.querySelector('.navbar');
-const mynavbar = document.querySelector('#mynavbar');
-const tel = document.querySelector('#tel');
+function toggleDetails(headerElement) {
+    const details = headerElement.nextElementSibling;
+    const button = headerElement.querySelector('.toggle-btn');
+    
+    // Toggle visibility
+    details.classList.toggle('show');
 
-function navbarScrolled() {
-    if (window.scrollY > 100) {
-        navbar.classList.add('navbar-dark');
-        navbar.classList.remove('navbar-light');
-        if (mynavbar) {
-            mynavbar.classList.add('mynavbar-scrolled');
-        }
-        if (tel) {
-            tel.classList.add('tel-scrolled');
-        }
-    } else {
-        navbar.classList.remove('navbar-dark');
-        navbar.classList.add('navbar-light');
-        if (mynavbar) {
-            mynavbar.classList.remove('mynavbar-scrolled');
-        }
-        if (tel) {
-            tel.classList.remove('tel-scrolled');
-        }
-    }
+    // Toggle the button rotation
+    button.classList.toggle("expanded");
 }
-
-window.addEventListener('load', navbarScrolled);
-window.addEventListener('scroll', navbarScrolled);
-
-// Gallery carousel
-$(document).ready(function () {
-    $("#show-carousel").click(function () {
-        $(".row-cols-3").owlCarousel({
-            loop: true,
-            margin: 10,
-            nav: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 3
-                },
-                1000: {
-                    items: 5
-                }
-            }
-        });
-    });
-});
